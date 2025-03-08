7ca020d1d9c95d0453f3d8ce94543054099152e5
makironese <140657248+makironese@users.noreply.github.com>
2025-03-08 13:31:36 -0300

    s

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 document.getElementById("AbrirPaginaBiologia").addEventListener("click", function() {
     window.open.href = "Biologia";
 });
-document.querySelector(".dropdown button").addEventListener("click", function() {
-    const menu = document.querySelector(".dropdown-content");
-    menu.style.display = menu.style.display === "block" ? "none" : "block";
-});
